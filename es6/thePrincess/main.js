965a83dc0b3203c1935057165eac32bf90529f67
Nathan Sutherland <nathan.sutherland17@gmail.com>
2020-07-31 12:28:22 -0600

    it works

--- es6/thePrincess/main.js
+++ es6/thePrincess/main.js
@@ -1,80 +1,125 @@
 'use strict'
 
-let name = ["Mario", "Luigi"]
+let names = ["Mario", "Luigi"]
 let totalCoins = 0
 let stati = ["Powered Up", "Big", "Small", "Dead"]
 let hasStar = false
 let gameActive = true
-let status = "Big"
-let namePicked
+let stars
 
+function createRandomName() {
+    let sum
+    let name
+    sum = Math.floor(Math.random() * 2)
+    name = names[sum]
+    return name
+}
+
+function createRandomNameStatus() {
+    let sum
+    let status
+    let stati = ["Powered Up", "Big", "Small"]
+    sum = Math.floor(Math.random() * 3)
+    status = stati[sum]
+    return status
+}
 
 class Player {
-    constructor(namePicked, totalCoins, status, hasStar) {
+    constructor(name, totalCoins, status, hasStar) {
         this.name = name
         this.totalCoins = totalCoins
         this.status = status
         this.hasStar = hasStar
     }
     setName(namePicked) {
-        let sum
-        sum = Math.floor(Math.random() * 2)
-        namePicked = name[sum]
-        return namePicked
+        this.name = namePicked
     }
-    gotHit = () => status = stati[(stati.indexOf(status) - 1)]
+    gotHit = () => {
+        if (this.status === "Powered Up") {
+            if (this.hasStar === true) {
+                console.log("Your star protected you.")
+                this.hasStar = false
+                this.status = this.status
+            } else {
+                this.status = stati[(stati.indexOf(this.status) + 1)]
+            }
+        } else {
+            this.status = stati[(stati.indexOf(this.status) + 1)]
+            this.hasStar = false
+        }
+    }
     gotPowerup() {
-        status = stati[(stati.indexOf(status) + 1)]
-        if (status === "Powered Up") {
-            hasStar = true
+        this.status = stati[(stati.indexOf(this.status) - 1)]
+        if (this.status == undefined) {
+            console.log("Congratulations! You got a star.")
+            this.hasStar = true
+            this.status = "Powered Up"
         }
     }
-    addCoin = () => totalCoins += 1
+    addCoin = () => this.totalCoins += 1
     gameActive(){
-        if (status = undefined){
-            break
-            return status
-        }
+        console.log(
+            `
+            Name: ${this.name}
+            Status: ${this.status}
+            Total Coins: ${this.totalCoins}
+            Game over.
+            `
+        )
+        process.exit()
     }
     print() {
-        if (hasStar === true) {
+        if (this.hasStar === true) {
             console.log(
                 `
-                Name: ${name}
-                Status: ${status}
-                Total Coins: ${totalCoins}
+                Name: ${this.name}
+                Status: ${this.status}
+                Total Coins: ${this.totalCoins}
                 You have a star!
                 `
             )
         } else {
             console.log(
                 `
-                Name: ${name}
-                Status: ${status}
-                Total Coins: ${totalCoins}
-                You have a star!
+                Name: ${this.name}
+                Status: ${this.status}
+                Total Coins: ${this.totalCoins}
                 `
             )
         }  
     }
 }
 
+
 const randomRange = player => {
     let sum
-    sum = Math.floor(Math.random() * 3)
-    if (sum === 0) {
-        player.gotHit()
-    } else if (sum === 1) {
-        player.gotPowerup()
-    } else if (sum === 2) {
-        player.addCoin()
-    } else if (gameActive === false) {
-        return `Game Over`
+    if (player.hasStar === false) {
+        sum = Math.floor(Math.random() * 3)
+        if (sum === 0) {
+            player.gotHit()
+        } else if (sum === 1) {
+            player.gotPowerup()
+        } else if (sum === 2) {
+            player.addCoin()
+        } 
+        if (player.status === "Dead") {
+            player.gameActive()
+        }
+    } else if (player.hasStar === true) {
+        sum = Math.floor(Math.random() * 2)
+        if (sum === 0) {
+            player.gotHit()
+        } else if (sum === 1) {
+            player.addCoin()
+        } 
+        if (player.status === "Dead") {
+            player.gameActive()
+        }
     }
     player.print()
 }
 
-const createPlayer = new Player(name, 0, status, false)
+const createPlayer = new Player(createRandomName(), 0, createRandomNameStatus(), false)
 
 setInterval (() => {
     if(gameActive = true){

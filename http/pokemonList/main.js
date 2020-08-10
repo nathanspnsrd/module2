6714d43c5e565a8cebc70aeddc6a0f7317bb339b
Nathan Sutherland <nathan.sutherland17@gmail.com>
2020-08-10 10:59:19 -0600

    back from the cabin

---
+++ http/pokemonList/main.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const xhr = new XMLHttpRequest()
+
+xhr.open("GET", "https://api.vschool.io/pokemon", true)
+xhr.send()
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        const jsonData = xhr.responseText
+        const data = JSON.parse(jsonData)
+        showData(data.results)
+    }
+}
+
+function showData(arr) {
+    for(let i = 0; i < arr.length; i++){
+        const h1 = document.createElement('h1')
+        h1.textContent = arr[i].name
+        document.body.appendChild(h1)
+    }
+}
